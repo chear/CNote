57d1e80d606931609a0e2be883a4372a06d32bc1
Chear.Huang <chear.huang@mitrastar.cn>
2022-01-07 15:20:32 +0800

    clean diff bewteen 'char *str' and 'char str[]'

--- 3_array_test.c
+++ 3_array_test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 
 int main (){
     int a[5] = {0};
@@ -10,5 +11,27 @@ int main (){
     for (i = 1 ;i <= 5 ; i++)
         printf(" a[%d] = %d  \n",i, a[i]);
 
+    /* 'str1[]' working fine , but '*str' not work, 
+     * because '*str' did not malloc , it just point on stack, can not change any think within it.
+     */
+    char *str = "hellow the name";
+    char str1[] = "0x30 0x30 0x30 0x30 0x30 0x30 0x30 0x30 0x33 0x37";
+    char *p;
+    p = NULL;  
+    p = strtok(str1, " ");   
+    while(p){
+        printf("p= %s\n",p);
+        p = strtok(NULL, " ");
+    }
+
+    /* compile success, but can not run */
+    char *result = NULL;
+    result = strtok(str, " ");
+    while( result != NULL )
+    {
+        puts(result);
+        result = strtok(NULL, " ");
+    }
+    printf("Program Finished\n\n");
     return 0;
 }
